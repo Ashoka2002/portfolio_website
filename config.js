d08366e82e3d0082df033aac4b0b878da7cc7750
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-23 20:17:08 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,79 @@
+const projects = [
+  {
+    projectNo: 1,
+    projectTitle: "Smart-Brain-App",
+    description: `This is a react project that uses the Clarify API to detect faces in images. I created the back-end using Node.js for sign-in, registration, and image count, and I also use the API in the back-end for face detection logic. For the database, I use Postgres for storing user data and connect it to the server using the Knex library. and some packages that I use are Bcrypt, Tackyons, Express, etc.`,
+    projectLink: "https://smartbrain-n893.onrender.com",
+    projectCodeLink: "https://github.com/Ashoka2002/smart-brain",
+    projectImgLink: "./img/smart-brain.png",
+  },
+  {
+    projectNo: 2,
+    projectTitle: "Youtube_Clone",
+    description: `
+   1. Developed using React and leverages React Router for seamless navigation.
+   2. Utilizes the Rapid API to access real-time data from YouTube.
+   3. Implements styling with the help of Tailwind CSS for a modern and responsive design.
+   4.  Manages the state of the application efficiently using the React Context API.
+   5.  Ensures responsiveness across various devices, providing a consistent user experience
+   `,
+    projectLink: "https://youtube-clone-by-ashok.netlify.app/",
+    projectCodeLink: "https://github.com/Ashoka2002/Youtube-Clone",
+    projectImgLink: "./img/youtube.png",
+  },
+  {
+    projectNo: 3,
+    projectTitle: "Forkify",
+    description: `This website built with HTML, CSS, and JavaScript, it
+    follows the Model-View-Control (MVC) architecture,
+    making it highly organized and scalable with classes.
+    Enjoy the freedom to add your unique recipes,
+    discover random recipes, and conveniently bookmark
+    your favorites. Plus, have complete control over serving
+    sizes for a truly personalized culinary experience.
+    NOTE:This site is not responsive for small devices.
+    `,
+    projectLink: "https://forkify-by-ashok.netlify.app/",
+    projectCodeLink: "https://github.com/Ashoka2002/forkify",
+    projectImgLink: "./img/forkify.png",
+  },
+  {
+    projectNo: 4,
+    projectTitle: "Mapty",
+    description: `
+    I developed a Workout Tracker project while taking "The Complete JavaScript Course 2023 (Udemy)." course using Html,Css and Javascript. This app uses Leaflet for interactive workout maps and local storage to store user data. Please note that it's designed for desktops and lacks responsiveness on smaller devices.
+    `,
+    projectLink: "https://ashoka2002.github.io/mapty/",
+    projectCodeLink: "https://github.com/Ashoka2002/mapty/tree/main",
+    projectImgLink: "./img/mapty.png",
+  },
+  {
+    projectNo: 5,
+    projectTitle: "Linear_Color_Generator",
+    description: `
+    Simple web project that generate background color.
+    `,
+    projectLink: "https://ashoka2002.github.io/background-generator/",
+    projectCodeLink: "https://github.com/Ashoka2002/background-generator",
+    projectImgLink: "./img/bg-gen.png",
+  },
+  {
+    projectNo: 6,
+    projectTitle: "Guess My Number (Game)",
+    description: `Number guess game using simple Html,Css and Javascript(Non Responsive for small devices)`,
+    projectLink: "https://ashoka2002.github.io/Guess_my_number/",
+    projectCodeLink: "https://github.com/Ashoka2002/Guess_my_number",
+    projectImgLink: "./img/GMN.png",
+  },
+];
+
+//////Skill Section//
+const skills = [
+  { skillName: "HTML", skillPercent: 74 },
+  { skillName: "CSS", skillPercent: 72 },
+  { skillName: "javascript", skillPercent: 78 },
+  { skillName: "nodejs", skillPercent: 46 },
+  { skillName: "reactjs", skillPercent: 70 },
+  { skillName: "postgresql", skillPercent: 47 },
+  { skillName: "c++", skillPercent: 50 },
+];
---
+++ script.js
@@ -0,0 +1,126 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
+const header = document.querySelector("header");
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 100;
+    const height = sec.offsetHeight;
+    const id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+      });
+      //active section for animation on scroll
+      sec.classList.add("show-animate");
+    }
+
+    //for repeats animation on scroll
+    // else {
+    //   sec.classList.remove("show-animate");
+    // }
+  });
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+
+  //footer animation
+  document
+    .querySelector("footer")
+    .classList.toggle("show-animate", this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+};
+
+///////Projects
+
+function getProject(project) {
+  return `
+  <div class="project-container">
+        <div class="project-content">
+          <h2>Project <span>${project.projectNo}</span></h2>
+          <h3>Title: <span>${project.projectTitle}</span></h3>
+          <p>
+           ${project.description}
+          </p>
+          <div class="btn-box">
+            <!-- <span class="animate" style="--i: 5"></span> -->
+            <a target="_blank" href="${project.projectLink}" class="btn">See Live</a>
+            <a target="_blank" href="${project.projectCodeLink}" class="btn">Source Code</a>
+          </div>
+        </div>
+
+        <div class="container">
+          <div class="card">
+          <a href=${project.projectImgLink} target="_blank">
+            <img
+              src=${project.projectImgLink}
+              alt="Project Image"
+            />
+            </a>
+          </div>
+        </div>
+      </div>
+  `;
+}
+
+function renderProjects() {
+  const projctDiv = document.getElementById("projects");
+  projects.forEach((proj) => {
+    const projectEl = getProject(proj);
+    projctDiv.insertAdjacentHTML("beforeend", projectEl);
+  });
+}
+
+renderProjects();
+
+//////Skills
+
+function renderSkills() {
+  //Remeber there is a array that content skills in config.js
+  const skillBox = document.querySelector(".skills-content");
+  skills.forEach((skll) => {
+    const html = `
+    <div class="progress">
+    <h3>${skll.skillName} <span>${skll.skillPercent}%</span></h3>
+    <div class="bar"><span style="width:${skll.skillPercent}%"></span></div>
+    </div>
+    `;
+    skillBox.insertAdjacentHTML("beforeend", html);
+  });
+}
+
+renderSkills();
+
+/////Dark mode
+const root = document.documentElement;
+const appearence = document.getElementById("appearance");
+
+appearence.addEventListener("change", function () {
+  if (this.checked) {
+    root.style.setProperty("--bg-color", "#fafafa");
+    root.style.setProperty("--second-bg-color", "#d2d3db");
+    root.style.setProperty("--text-color", "#081b29");
+  } else {
+    root.style.setProperty("--bg-color", "#081b29");
+    root.style.setProperty("--second-bg-color", "#112e42");
+    root.style.setProperty("--text-color", "#ededed");
+  }
+});
+///////Vanila tilt effect
+
+VanillaTilt.init(document.querySelectorAll(".card"), {
+  max: 5,
+  speed: 200,
+  glare: true,
+  "max-glare": 0.2,
+});
